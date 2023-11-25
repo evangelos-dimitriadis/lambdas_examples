ec9c18ac20541f28c07bfbd81c551917a80cac91
Dimitriadis <evangelos.dimitriadis@protonmail.com>
2023-11-25 15:49:59 +0100

    Improves and cleans the terraform files

--- outputs.tf
+++ outputs.tf
@@ -1,42 +1,20 @@
-output "hello-world" {
-  description = "Print a Hello World text output"
-  value       = "Hello World"
-}
 output "vpc_id" {
   description = "Output the ID for the primary VPC"
   value       = aws_default_vpc.default.id
 }
-
 output "aws_availability_zones" {
   description = "Subnets"
   value       = data.aws_availability_zones.available.id
 }
-
 output "secret" {
   description = "The id of the DB key"
   value       = aws_db_instance.postgres.master_user_secret[0]["secret_arn"]
 }
-
 output "db_host" {
   description = "The host of the DB"
   value       = aws_db_instance.postgres.address
 }
-
-# output "iam_role" {
-#   value = aws_iam_policy.lambda_iam.arn
-# }
-
-# output "vpc_information" {
-#   description = "VPC Information about Environment"
-#   value       = "VPC has an ID of ${aws_vpc.vpc.id}"
-# }
-# output "public_ip" {
-#   value = module.server.public_ip
-# }
-# output "asg_group_size" {
-#   value = module.autoscaling.autoscaling_group_max_size
-# }
-# output "so_secret" {
-#   value     = var.so_secret
-#   sensitive = true
-# }
+output "ip" {
+  description = "Public IP of this machine"
+  value       = data.http.ip.response_body
+}
--- providers/terraform.tf
+++ providers/terraform.tf
@@ -5,5 +5,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~>5.26.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "~>3.4.0"
+    }
   }
 }
